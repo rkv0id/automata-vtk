c7f614330bdef63e2214d32fb7394405fdb665b6
ramyk <ramy.kader.rk@gmail.com>
2021-03-23 03:36:54 +0100

    support mp4 export

--- visualize.py
+++ visualize.py
@@ -1,6 +1,7 @@
-from numpy import stack, zeros
-from matplotlib.pyplot import imsave
+from numpy import stack, zeros, max, min
+from matplotlib.pyplot import imsave, cm
 from array2gif import write_gif
+from skvideo.io import FFmpegWriter
 
 def image_seq(grids, outfolder="out_gpu", cmap="jet"):
     print("Generating image sequence...")
@@ -19,3 +20,17 @@ def gif(grids, fps=25, outname="out_gpu"):
                 zeros(grid.shape)], axis=2))
     write_gif(frames, outname+".gif", fps=fps)
     print("GIF created: " + outname + ".gif")
+
+def mp4(grids, fps=25, outname="out_gpu"):
+    w = FFmpegWriter(outname + ".mp4", inputdict={"-r": str(fps)})
+    print("Generating MP4 video...")
+    for frame in grids:
+        mn, mx = min(frame), max(frame)
+        frame -= mn
+        frame /= (mx-mn)
+        frame = cm.jet(frame.transpose())
+        frame *= 255
+        frame = frame.astype("uint8")
+        w.writeFrame(frame)
+    w.close()
+    print("MP4 created: " + outname + ".mp4")
